ef3520ee8e19fb190f408ab6b1eb55a3b438acb4
shagun-nickelfox <shagun.bansal@nickelfox.com>
2023-12-10 17:20:59 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Nickelfox on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
